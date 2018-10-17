29f1115896fe0cb73142573fa4296e217cba24d8
Tamizhvendan S <tamizh88@gmail.com>
2018-10-18 01:07:04 +0530

    cleanup old code

--- clojure/restful-crud/src/restful_crud/book.clj
+++ clojure/restful-crud/src/restful_crud/book.clj
@@ -1,10 +1,7 @@
 (ns restful-crud.book
   (:require [schema.core :as s]
             [restful-crud.models.book :refer [Book]]
-            [toucan.db :as db]
             [restful-crud.restful :as restful]
-            [ring.util.http-response :refer [ok not-found created]]
-            [compojure.api.sweet :refer [GET POST PUT DELETE]]
             [restful-crud.string-util :as str]))
 
 (defn valid-book-title? [title]
@@ -16,51 +13,6 @@
 (s/defschema BookRequestSchema
   {:title (s/constrained s/Str valid-book-title?)
    :year_published (s/constrained s/Int valid-year-published?)})
-
-(defn id->created [id]
-  (created (str "/books/" id) {:id id}))
-
-(defn create-book-handler [create-book-req]
-  (-> (db/insert! Book create-book-req)
-      :id
-      id->created))
-
-(defn delete-book-handler [book-id]
-  (db/delete! Book :id book-id)
-  (ok))
-
-(defn get-books-handler []
-  (ok (db/select Book)))
-
-(defn book->response [book]
-  (if book
-    (ok book)
-    (not-found)))
-
-(defn get-book-handler [book-id]
-  (-> (Book book-id)
-      book->response))
-
-(defn update-book-handler [id update-book-req]
-  (db/update! Book id update-book-req)
-  (ok))
-
-(def book-routes
-  [(POST "/books" []
-     :body [create-book-req BookRequestSchema]
-     (create-book-handler create-book-req))
-   (GET "/books" []
-     (get-books-handler))
-   (GET "/books/:id" []
-     :path-params [id :- s/Int]
-     (get-book-handler id))
-   (PUT "/books/:id" []
-     :path-params [id :- s/Int]
-     :body [update-book-req BookRequestSchema]
-     (update-book-handler id update-book-req))
-   (DELETE "/books/:id" []
-     :path-params [id :- s/Int]
-     (delete-book-handler id))])
 
 (def book-entity-route
   (restful/resource {:model Book
--- clojure/restful-crud/src/restful_crud/user.clj
+++ clojure/restful-crud/src/restful_crud/user.clj
@@ -3,10 +3,7 @@
             [restful-crud.models.user :refer [User]]
             [buddy.hashers :as hashers]
             [clojure.set :refer [rename-keys]]
-            [toucan.db :as db]
             [restful-crud.restful :as restful]
-            [ring.util.http-response :refer [ok not-found created]]
-            [compojure.api.sweet :refer [GET POST PUT DELETE]]
             [restful-crud.string-util :as str]))
 
 (defn valid-username? [name]
@@ -20,58 +17,9 @@
    :password (s/constrained s/Str valid-password?)
    :email (s/constrained s/Str str/email?)})
 
-(defn id->created [id]
-  (created (str "/users/" id) {:id id}))
-
 (defn canonicalize-user-req [user-req]
   (-> (update user-req :password hashers/derive)
       (rename-keys {:password :password_hash})))
-
-(defn create-user-handler [create-user-req]
-  (->> (canonicalize-user-req create-user-req)
-       (db/insert! User)
-       :id
-       id->created))
-
-(defn delete-user-handler [user-id]
-  (db/delete! User :id user-id)
-  (ok))
-
-(defn get-users-handler []
-  (->> (db/select User)
-       (map #(dissoc % :password_hash))
-       ok))
-
-(defn user->response [user]
-  (if user
-    (ok user)
-    (not-found)))
-
-(defn get-user-handler [user-id]
-  (-> (User user-id)
-      (dissoc :password_hash)
-      user->response))
-
-(defn update-user-handler [id update-user-req]
-  (db/update! User id (canonicalize-user-req update-user-req))
-  (ok))
-
-(def user-routes
-  [(POST "/users" []
-     :body [create-user-req UserRequestSchema]
-     (create-user-handler create-user-req))
-   (GET "/users" []
-     (get-users-handler))
-   (GET "/users/:id" []
-     :path-params [id :- s/Int]
-     (get-user-handler id))
-   (PUT "/users/:id" []
-     :path-params [id :- s/Int]
-     :body [update-user-req UserRequestSchema]
-     (update-user-handler id update-user-req))
-   (DELETE "/users/:id" []
-     :path-params [id :- s/Int]
-     (delete-user-handler id))])
 
 (def user-entity-route
   (restful/resource {:model User
