e5460f35143641d815bf36989f0322f45a892bfe
Tamizhvendan S <tamizh88@gmail.com>
2018-12-15 21:38:54 +0530

    update suave sample

--- fsharp/ModelBinding/SuaveSample/src/SuaveSample/Program.fs
+++ fsharp/ModelBinding/SuaveSample/src/SuaveSample/Program.fs
@@ -1,19 +1,72 @@
 module SuaveSample
 
 open System
-open System.Threading
 open Suave
+open Suave.Operators
+open Suave.Filters
+open FsConfig
+
+type DealsCategory =
+| AllDeals
+| AllEBooks
+| ActionAndAdventure
+| Media
+| Fiction
+
+type Language =
+| English
+| Hindi
+| Tamil
+
+type Rating =
+| Five
+| FourAndAbove
+| ThreeAndAbove
+
+type SearchFilter = {
+  Language : Language list
+  Rating : Rating option
+}
+
+type Search = {
+  Category : DealsCategory
+  Filter : SearchFilter
+}
+
+let queryStringsMap (request : HttpRequest) =
+  request.query
+  |> List.groupBy fst
+  |> List.map (fun (x, keyVals) -> 
+                (x, keyVals |> List.map snd |> List.choose id |> String.concat ","))
+  |> Map.ofList
+
+type HttpQueryStringsProvider(request : HttpRequest) =
+    let queryStringsMap = queryStringsMap request
+
+    interface IConfigReader with
+      member __.GetValue name =
+        Map.tryFind name queryStringsMap
+
+let private camelCaseCanonicalizer _ (name : string) =
+    name
+    |> String.mapi (fun i c ->
+      if (i = 0) then Char.ToLowerInvariant c else c)
+
+let bindQueryStrings<'T> (request : HttpRequest) =
+  let queryStringsProvider = new HttpQueryStringsProvider(request)
+  parse<'T> queryStringsProvider camelCaseCanonicalizer ""
+  |> Result.mapError (fun e -> e.ToString())
+
+let getBooks ctx = async {
+  let search = bindQueryStrings<Search> ctx.request
+  printfn "%A" search
+  return! Successful.OK "Todo" ctx
+}
+
+let app =
+  path "/books" >=> getBooks
 
 [<EntryPoint>]
 let main argv = 
-    let cts = new CancellationTokenSource()
-    let conf = { defaultConfig with cancellationToken = cts.Token }
-    let listening, server = startWebServerAsync conf (Successful.OK "Hello World")
-
-    Async.Start(server, cts.Token)
-    printfn "Make requests now"
-    Console.ReadKey true |> ignore
-
-    cts.Cancel()
-
-    0 // return an integer exit code
+  startWebServer defaultConfig app
+  0 // return an integer exit code
