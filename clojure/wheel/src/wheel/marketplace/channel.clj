0cee8d1a535267a2760bc7a01828d0316b4766f0
Tamizhvendan S <tamizh88@gmail.com>
2019-10-02 05:26:19 +0530

    fix channel id

--- clojure/wheel/src/wheel/marketplace/channel.clj
+++ clojure/wheel/src/wheel/marketplace/channel.clj
@@ -1,5 +1,5 @@
 (ns wheel.marketplace.channel
   (:require [clojure.spec.alpha :as s]))
 
-(s/def ::id pos-int?)
+(s/def ::id (complement clojure.string/blank?))
 (s/def ::name #{:tata-cliq :amazon :flipkart})
