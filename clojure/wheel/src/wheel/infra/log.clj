9832b4ea934968eef6050e9710ca424e293307fe
Tamizhvendan S <tamizh88@gmail.com>
2019-10-01 21:30:29 +0530

    rewrite json-output and minor fixes

--- clojure/wheel/src/wheel/infra/log.clj
+++ clojure/wheel/src/wheel/infra/log.clj
@@ -4,11 +4,10 @@
             [clojure.spec.alpha :as s]
             [wheel.middleware.event :as event]))
 
-(defn- json-output [{:keys [level msg_ instant]}]
+(defn- json-output [{:keys [msg_]}]
   (let [event (read-string (force msg_))]
-    (json/generate-string {:timestamp instant
-                           :level level
-                           :event event})))
+    (json/generate-string event)))
+
 (defn init []
   (timbre/merge-config! {:output-fn json-output}))
 
@@ -31,15 +30,15 @@
   (init)
   (timbre/info {:name :ranging/succeeded})
   (s/check-asserts true)
-  (write-all! [{:level :info :name :foo}])
+  (write! {:level :info :name :foo})
   (s/check-asserts false)
   (s/assert ::event/event {:level :info :name :foo})
   (write! {:name :ranging/succeeded
            :type :domain
            :level :info
            :channel-id 1
+           :timestamp "2019-10-01T21:56+05:30"
            :id #uuid "8e70f4df-fc3d-4474-8ee9-33f4c87bf934"
            :parent-id #uuid "20c84be2-7ec2-4127-8d39-a9cb4065a9ce"
-           :channel-name :tata-cliq
-           :payload {}})
+           :channel-name :tata-cliq})
   )
